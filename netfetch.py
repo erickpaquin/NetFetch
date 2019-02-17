2a821db42af3b2af7977aa632fa550e01beddbe8
Erick Paquin <info@erickpaquin.com>
2019-02-17 16:59:13 -0500

    Initial commit. Added description header

---
+++ netfetch.py
@@ -0,0 +1,14 @@
+############################################################
+# netfetch.py
+# 
+# This program goes out via ssh or telnet and fetches config
+# files of Cisco (and eventually other manufacturers)
+# routers and switches.
+# 
+# Created by Erick Paquin (info@erickpaquin.com)
+#
+# Version 1.0 (February 17 2019)                                    
+# 
+############################################################
+
+
