9c6273657b26262a1dc3884f68a16238a5e730be
Erick Paquin <info@erickpaquin.com>
2019-03-22 20:47:31 +0000

    add hosts.csv and cisco-commands.txt to repo

--- netfetch.py
+++ netfetch.py
@@ -1,20 +1,20 @@
 ########################################################################
 # netfetch.py
-# 
-# This program goes out via ssh or telnet and fetches config
+#
+# THIS program goes out via ssh or telnet and fetches config
 # files of Cisco (and eventually other manufacturers)
 # routers and switches.
-# 
-# Created by Erick Paquin (info@erickpaquin.com)
 #
-# Version 1.0 (February 17 2019)                                    
-# 
+# Created by Erick Paquin, info@erickpaquin.com
+#
+# Version 1.0 (last updated March 21 2019)
+#
 ############################################################
 
-import argparse  # module to parse command-line arguments to the program.
+import fileinput
+import telnetlib
+import netmiko
 
-
-# Setup the program arguments
-argparser = argparse.ArgumentParser()
-argparser.parse_args()
+for line in fileinput.input():
+        print(line)
 
