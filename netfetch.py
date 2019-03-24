0c71c09afb1c080f138fb93c472c090fe0641719
Erick Paquin <info@erickpaquin.com>
2019-03-24 21:18:09 +0000

    completed first working pass

--- netfetch.py
+++ netfetch.py
@@ -12,10 +12,40 @@
 #
 ############################################################
 
-import fileinput
+#import fileinput
+import sys
+import csv
+import re
 import telnetlib
-import netmiko
+from netmiko import Netmiko
 
-for line in fileinput.input():
-        print(line)
+# Verify that the hostfile has been passed to the program, if not exit.
+if len(sys.argv) < 2:
+    print("You forgot to specify your hosts file?")
 
+# If hostfile has been passed, create a csvreader and start reading lines.
+else:
+    with open(sys.argv[1]) as csvfile:
+        device_read = 0
+        hostfile = csv.reader(csvfile)
+        hostfile.__next__() # skip the header line.
+        for row in hostfile:
+            net_connect = Netmiko(
+                row[0],
+                username=row[1],
+                password=row[2],
+                device_type=row[3],
+                port=row[4],
+            )
+            with open(row[5], 'r') as commands:
+                for line in commands:
+                    line = line.rstrip('\r\n')
+                    output = net_connect.send_command(line)
+                    filename = row[0] + "_" + line.replace(" ", "_") + ".txt"
+                    writefile = open(filename, 'w')
+                    writefile.write(output)
+                    writefile.close()
+            net_connect.disconnect()
+            device_read += 1
+            print(device_read, "devices done...")
+    print("Process complete.", device_read, "devices read.")
