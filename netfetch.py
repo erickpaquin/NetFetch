966a8760473795a4792c5bdd17affe714c33357b
Erick Paquin <info@erickpaquin.com>
2019-03-25 15:35:20 +0000

    Added epaquinlib library file + made code work with threads

--- netfetch.py
+++ netfetch.py
@@ -12,9 +12,11 @@
 #
 ############################################################
 
+import threading
+from datetime import datetime
 import sys
 import csv
-from netmiko import Netmiko
+from epaquinlib import get_devices
 
 # Verify that the hostfile has been passed to the program, if not exit.
 if len(sys.argv) < 2:
@@ -22,29 +24,17 @@ if len(sys.argv) < 2:
 
 # If hostfile has been passed, create a csvreader and start reading lines.
 else:
+    start_time = datetime.now()
+    device_read = 0
     with open(sys.argv[1]) as csvfile:
-        device_read = 0
         hostfile = csv.reader(csvfile)
         hostfile.__next__() # skip the header line.
         for row in hostfile:
-            net_connect = Netmiko(
-                row[0],
-                username=row[1],
-                password=row[2],
-                secret=row[3],
-                device_type=row[4],
-                port=row[5],
-            )
-            net_connect.enable()
-            with open(row[6], 'r') as commands:
-                for line in commands:
-                    line = line.rstrip('\r\n')
-                    output = net_connect.send_command(line)
-                    filename = row[0] + "_" + line.replace(" ", "_") + ".txt"
-                    writefile = open(filename, 'w')
-                    writefile.write(output)
-                    writefile.close()
-            #net_connect.disconnect()
+            my_thread = threading.Thread(target=get_devices, args=(row,))
+            my_thread.start()
+    main_thread = threading.currentThread()
+    for some_thread in threading.enumerate():
+        if some_thread != main_thread:
             device_read += 1
-            print(device_read, "devices done...")
-    print("Process complete.", device_read, "devices read.")
+            some_thread.join()
+    print("Process complete.", device_read, "devices read in", str(datetime.now() - start_time), "seconds")
