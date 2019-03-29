9396ebd4ddaadde9160d152d11e3c4b0076be43d
Erick Paquin <info@erickpaquin.com>
2019-03-29 18:48:14 +0000

    Coded basic TextFSM to CSV function

--- epaquinlib.py
+++ epaquinlib.py
@@ -1,4 +1,6 @@
 from netmiko import Netmiko
+import json
+import csv
 
 def get_devices(row):
     net_connect = Netmiko(
@@ -13,8 +15,14 @@ def get_devices(row):
     with open(row[6], 'r') as commands:
         for line in commands:
             line = line.rstrip('\r\n')
-            output = net_connect.send_command(line)
-            filename = row[0] + "_" + line.replace(" ", "_") + ".txt"
-            writefile = open(filename, 'w')
-            writefile.write(output)
-            writefile.close()
+            output = net_connect.send_command(line, use_textfsm=True)
+            #output = json.dumps(output)
+            #output_parsed = json.loads(output)
+            #print (output)
+            filename = row[0] + "_" + line.replace(" ", "_") + ".csv"
+            with open(filename, 'w') as resultFile:
+                for data in output:
+                    w = csv.DictWriter(resultFile, data.keys())
+                    w.writeheader()
+                    w.writerow(data)
+            resultFile.close()
